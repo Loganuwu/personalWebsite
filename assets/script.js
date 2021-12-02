28f249efa3e5494c3448619643113d94041f76d9
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2021-12-01 21:18:39 -0800

    updated nav bar to be clicked and change colors

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,35 @@
-function hoverScript() {
-    console.log("Testing hover");
-    document.getElementById("gitIcon").innerHTML = `<img src="./assets/logo-github.svg" alt="Github" style="padding-right: 20px;" width="150px" id="gitIcon">`
+function projectNavScript() {
+    document.getElementById("projectNav").style.color = "red"
+    document.getElementById("scrollspyProject").style.color = "red"
+    setTimeout(() => {
+        document.getElementById("projectNav").style.color = "white"
+        document.getElementById("scrollspyProject").style.color = "#64ffda"
+    }, 4000);
+    
 }
 
-document.getElementById("gitIcon").addEventListener("mouseover", hoverScript)
+function resumeNavScript() {
+    document.getElementById("resumeNav").style.color = "red"
+    document.getElementById("scrollspyResume").style.color = "red"
+    setTimeout(() => {
+        document.getElementById("resumeNav").style.color = "white"
+        document.getElementById("scrollspyResume").style.color = "#64ffda"
+    }, 4000);
+    
+}
+
+function contactNavScript() {
+    document.getElementById("contactNav").style.color = "red"
+    document.getElementById("scrollspyContact").style.color = "red"
+    setTimeout(() => {
+        document.getElementById("contactNav").style.color = "white"
+        document.getElementById("scrollspyContact").style.color = "#64ffda"
+    }, 4000);
+    
+}
+
+
+
+document.getElementById("projectNav").addEventListener("click", projectNavScript)
+document.getElementById("resumeNav").addEventListener("click", resumeNavScript)
+document.getElementById("contactNav").addEventListener("click", contactNavScript)
